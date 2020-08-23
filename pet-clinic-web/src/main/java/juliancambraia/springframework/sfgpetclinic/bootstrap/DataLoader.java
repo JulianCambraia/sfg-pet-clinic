3fcfee8d2865b1717ad2fc903f1463d1996fdb16
JulianCambraia <juliancambraia@gmail.com>
2020-08-23 13:37:45 -0300

    Adiciona Spring Conf. closes #22

--- pet-clinic-web/src/main/java/juliancambraia/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/juliancambraia/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import juliancambraia.springframework.sfgpetclinic.model.Owner;
 import juliancambraia.springframework.sfgpetclinic.model.Vet;
 import juliancambraia.springframework.sfgpetclinic.services.OwnerService;
 import juliancambraia.springframework.sfgpetclinic.services.VetService;
-import juliancambraia.springframework.sfgpetclinic.services.map.OwnerServiceMap;
-import juliancambraia.springframework.sfgpetclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,9 +13,9 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
