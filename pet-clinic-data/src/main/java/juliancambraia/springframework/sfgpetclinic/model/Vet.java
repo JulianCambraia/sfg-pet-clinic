93dc9363a2b69ec1e4a72a177faaabcdcbbf2b93
JulianCambraia <juliancambraia@gmail.com>
2020-08-29 18:59:41 -0300

    Cria a Entidade Speciality e atualiza Vet com um conjunto de Speciality. Closes #29

--- pet-clinic-data/src/main/java/juliancambraia/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/juliancambraia/springframework/sfgpetclinic/model/Vet.java
@@ -1,8 +1,21 @@
 package juliancambraia.springframework.sfgpetclinic.model;
 
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author juliancambraia
  */
 public class Vet extends Person {
+
+    private Set<Speciality> specialities = new HashSet<>();
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
