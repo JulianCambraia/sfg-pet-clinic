e6cf94d2e3724d80ef599aacdfb597682f1bc14e
JulianCambraia <juliancambraia@gmail.com>
2020-08-29 18:43:59 -0300

    Adiciona Visit atualiza Owner com o relacioamento de Pet. Closes #28

--- pet-clinic-data/src/main/java/juliancambraia/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/juliancambraia/springframework/sfgpetclinic/model/Owner.java
@@ -1,7 +1,20 @@
 package juliancambraia.springframework.sfgpetclinic.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author juliancambraia
  */
 public class Owner extends Person {
+
+    Set<Pet> pets = new HashSet<>();
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
