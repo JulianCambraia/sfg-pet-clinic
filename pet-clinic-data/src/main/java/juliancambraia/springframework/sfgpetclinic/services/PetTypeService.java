924bce501eba784c4e35600519f2467ac418bb33
JulianCambraia <juliancambraia@gmail.com>
2020-08-29 19:26:28 -0300

    Adiciona service PetTypeService e PetTypeMapService. Closes #31

---
+++ pet-clinic-data/src/main/java/juliancambraia/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package juliancambraia.springframework.sfgpetclinic.services;
+
+import juliancambraia.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
