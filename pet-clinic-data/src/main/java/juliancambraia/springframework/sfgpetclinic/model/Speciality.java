36d75ad0ea19d35ed9087efa427de12e6a37d98f
JulianCambraia <juliancambraia@gmail.com>
2020-09-21 19:00:41 -0300

    Updated Vets and Speciality with JPA entity mappings. Closes #39

--- pet-clinic-data/src/main/java/juliancambraia/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/juliancambraia/springframework/sfgpetclinic/model/Speciality.java
@@ -1,6 +1,18 @@
 package juliancambraia.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author juliancambraia
+ */
+
+@Entity
+@Table(name = "speciality")
 public class Speciality extends BaseEntity {
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
