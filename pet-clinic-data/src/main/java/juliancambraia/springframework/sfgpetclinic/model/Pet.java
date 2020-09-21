05d5ff819666160aef849531ed3b87bfe83b1d8b
JulianCambraia <juliancambraia@gmail.com>
2020-09-21 19:21:02 -0300

    Updated Visit and Pet with JPA entity mappings. Closes #40

--- pet-clinic-data/src/main/java/juliancambraia/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/juliancambraia/springframework/sfgpetclinic/model/Pet.java
@@ -1,11 +1,15 @@
 package juliancambraia.springframework.sfgpetclinic.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Entidade Pets
@@ -30,6 +34,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -61,5 +68,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
